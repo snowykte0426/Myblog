a015e357e54ccb05929f719d995c6ec93c3486ce
snowykte0426 <snowykte0426@naver.com>
2024-11-26 13:36:04 +0900

    style: 프로젝트 종속성 관리
    주석을 추가하고 종속성을 종류별로 구별하였습니다

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,13 +20,24 @@ repositories {
 }
 
 dependencies {
+	/* Spring Boot Starters */
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	/* JSON 처리 */
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+	/* Kotlin 런타임 및 리플렉션 */
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	/* 개발용 도구 */
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+	/* 데이터베이스 드라이버 */
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+
+	/* 테스트 관련 */
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
