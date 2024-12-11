8306d65f5dd8afe8ca050a6dc1c3be92c717be0d
snowykte0426 <snowykte0426@naver.com>
2024-12-11 20:17:29 +0900

    chore: AWS SDK 종속성 추가
    AWS에서 제공하는 기능을 적용하기 위하여 필요한 종속성을 추가하였습니다

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,24 +20,32 @@ repositories {
 }
 
 dependencies {
-	/* Spring Boot Starters */
+	// Spring Boot Starters
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-cache")
 
-	/* JSON 처리 */
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-
-	/* Kotlin 런타임 및 리플렉션 */
+	// Kotlin
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-	/* 개발용 도구 */
+	// JSON 처리
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+	// Caffeine
+	implementation("com.github.ben-manes.caffeine:caffeine")
+
+	// 개발용 도구
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-	/* 데이터베이스 드라이버 */
+	// 데이터베이스 드라이버
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 
-	/* 테스트 관련 */
+	// AWS SDK
+	implementation("com.amazonaws:aws-java-sdk-s3:1.12.691")
+
+	// 테스트 관련
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
