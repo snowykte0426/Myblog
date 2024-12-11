6c1c5c5c7a9456caecdd0ef1a952c984d30a623a
snowykte0426 <snowykte0426@naver.com>
2024-12-11 20:19:47 +0900

    feat: 글 수정 로직 구현 및 관련 DTO 정의
    글을 수정하는 비즈니스 로직을 구현하였습니다

---
+++ src/main/kotlin/com/nextech/myblog/api/domain/article/dto/request/PatchArticleRequest.kt
@@ -0,0 +1,8 @@
+package com.nextech.myblog.api.domain.article.dto.request
+
+class PatchArticleRequest {
+    var title: String? = null
+    var content: String? = null
+    var tag: String? = null
+    var image: String? = null
+}
---
+++ src/main/kotlin/com/nextech/myblog/api/domain/article/service/impl/PatchArticleServiceImpl.kt
@@ -0,0 +1,56 @@
+package com.nextech.myblog.api.domain.article.service.impl
+
+import com.nextech.myblog.api.domain.article.component.FindArticle
+import com.nextech.myblog.api.domain.article.component.ModifyArticle
+import com.nextech.myblog.api.domain.article.dto.response.ArticleResponse
+import com.nextech.myblog.api.domain.article.service.PatchArticleService
+import com.nextech.myblog.api.global.aws.service.FileUploadService
+import com.nextech.myblog.api.global.util.Base64DecodedMultipartFile
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class PatchArticleServiceImpl(
+    @Value("\${aws.s3.dev-url}")
+    private val devUrl: String,
+    private val findArticle: FindArticle,
+    private val modifyArticle: ModifyArticle,
+    private val fileUploadService: FileUploadService
+) : PatchArticleService {
+    override fun execute(id: Long, title: String?, content: String?, tag: String?, image: String?): ArticleResponse {
+        val article = findArticle.execute(id)
+        title?.let { article.title = it }
+        content?.let { article.content = it }
+        tag?.let { article.tag = it }
+        image?.let {
+            val decodedImage = Base64.getDecoder().decode(it.substringAfter("base64,"))
+            val multipartFile = Base64DecodedMultipartFile(decodedImage, "image/jpeg")
+            val uploadResult = fileUploadService.uploadFile(multipartFile).get()
+            val uploadedUrl = uploadResult.first
+            val uploadedFileName = uploadResult.second
+            println("Uploaded file URL: $uploadedUrl, Name: $uploadedFileName")
+            article.imageUrl = devUrl + "/" + uploadedFileName
+        }
+        article.id?.let {
+            article.imageUrl?.let { it1 ->
+                modifyArticle.execute(
+                    id = it,
+                    title = article.title,
+                    content = article.content,
+                    tag = article.tag,
+                    image = it1
+                )
+            }
+        }
+        return ArticleResponse(
+            id = article.id!!,
+            title = article.title,
+            content = article.content,
+            tag = article.tag,
+            createdAt = article.createdAt.toString(),
+            viewCount = article.viewCount,
+            imageUrl = article.imageUrl
+        )
+    }
+}
