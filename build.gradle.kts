c7f67c3229c95a5b1fffcaec0334bc310851ef78
snowykte0426 <snowykte0426@naver.com>
2024-12-20 14:42:33 +0900

    chore: ``noArg`` 옵션 추가
    Kotlin 개발 환경에 맞추어 생성자 관련 설정을 추가하였습니다

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,77 +1,83 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.4.0"
-	id("io.spring.dependency-management") version "1.1.6"
-	kotlin("plugin.jpa") version "1.9.25"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
+    id("org.springframework.boot") version "3.4.0"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "com.snowykte0426"
-version = "0.0.1"
+version = "0.0.2"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	// Spring Boot Starters
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-jdbc")
-	implementation("org.springframework.boot:spring-boot-starter-cache")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
+    // Spring Boot Starters
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-	// Kotlin
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    // Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
 
-	// JSON 처리
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    // JSON 처리
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
-	// Caffeine
-	implementation("com.github.ben-manes.caffeine:caffeine")
+    // Caffeine
+    implementation("com.github.ben-manes.caffeine:caffeine")
 
-	// 개발용 도구
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
+    // 개발용 도구
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-	// 데이터베이스 드라이버
-	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
-	runtimeOnly("com.mysql:mysql-connector-j")
+    // 데이터베이스 드라이버
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+    runtimeOnly("com.mysql:mysql-connector-j")
 
-	// AWS SDK v1
-	implementation("com.amazonaws:aws-java-sdk-s3:1.12.691")
+    // AWS SDK
+    implementation("com.amazonaws:aws-java-sdk-s3:1.12.691")
 
-	// Kotlin Coroutines
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.7.3")
+    // Kotlin Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.7.3")
 
-	// 테스트 관련
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude(group = "org.mockito")
-	}
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // 테스트 관련
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.mockito")
+    }
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
 }
 
 allOpen {
-	annotation("jakarta.persistence.Entity")
-	annotation("jakarta.persistence.MappedSuperclass")
-	annotation("jakarta.persistence.Embeddable")
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+noArg {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
