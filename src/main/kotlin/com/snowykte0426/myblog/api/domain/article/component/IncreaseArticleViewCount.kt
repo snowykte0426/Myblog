1e79c5619209643b6e4b37c0afe576aa6b28cec1
snowykte0426 <snowykte0426@naver.com>
2024-12-20 19:09:45 +0900

    feat: 조회수 증분 컴포넌트 정의
    조회수를 증분시키는 컴포넌트를 정의하였습니다

---
+++ src/main/kotlin/com/snowykte0426/myblog/api/domain/article/component/IncreaseArticleViewCount.kt
@@ -0,0 +1,13 @@
+package com.snowykte0426.myblog.api.domain.article.component
+
+import com.snowykte0426.myblog.api.domain.article.presentation.controller.dto.ArticleDto
+import com.snowykte0426.myblog.api.domain.article.repository.ArticleRepository
+import org.springframework.stereotype.Component
+
+@Component
+class IncreaseArticleViewCount(private val articleRepository: ArticleRepository) {
+    fun execute(article: ArticleDto): ArticleDto {
+        article.viewCount += 1
+        return articleRepository.save((article.toEntity())).toDto()
+    }
+}
