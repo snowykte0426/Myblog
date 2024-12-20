c5f9047e09d8cbbd979e260918600990c7110af2
snowykte0426 <snowykte0426@naver.com>
2024-12-20 14:43:10 +0900

    refactor: Entity 직접 반환에서 DTO 반환으로 변경
    Entity가 외부로 노출되지 않도록 DTO를 사용하여 반환하도록 하였습니다

--- src/main/kotlin/com/snowykte0426/myblog/api/domain/article/component/FindArticle.kt
+++ src/main/kotlin/com/snowykte0426/myblog/api/domain/article/component/FindArticle.kt
@@ -1,17 +1,28 @@
 package com.snowykte0426.myblog.api.domain.article.component
 
-import com.snowykte0426.myblog.api.domain.article.entity.Article
 import com.snowykte0426.myblog.api.domain.article.exception.ArticleNotFoundException
+import com.snowykte0426.myblog.api.domain.article.presentation.controller.dto.ArticleDto
 import com.snowykte0426.myblog.api.domain.article.repository.ArticleRepository
 import org.springframework.stereotype.Component
 
 @Component
 class FindArticle(private val articleRepository: ArticleRepository) {
-    fun execute(): List<Article> {
-        return articleRepository.findAll()
+    fun execute(): List<ArticleDto> {
+        return articleRepository.findAll().map {
+            ArticleDto(
+                id = it.id!!,
+                title = it.title,
+                content = it.content,
+                tag = it.tag,
+                createdAt = it.createdAt.toString(),
+                viewCount = it.viewCount,
+                imageUrl = it.imageUrl
+            )
+        }
     }
 
-    fun execute(id: Long): Article {
+    fun execute(id: Long): ArticleDto {
         return articleRepository.findById(id).orElseThrow { ArticleNotFoundException("Article with ID $id not found") }
+            .toDto()
     }
 }
