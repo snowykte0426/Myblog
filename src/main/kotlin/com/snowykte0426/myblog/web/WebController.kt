cf645cd8397d12c93b0a05bd84b621fceda713f1
snowykte0426 <snowykte0426@naver.com>
2024-12-20 19:10:20 +0900

    refactor: 글 세부정보 페이지 수정
    글의 세부정보 페이지를 수정하였습니다

--- src/main/kotlin/com/snowykte0426/myblog/web/WebController.kt
+++ src/main/kotlin/com/snowykte0426/myblog/web/WebController.kt
@@ -2,16 +2,17 @@ package com.snowykte0426.myblog.web
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
-class WebController{
+class WebController {
     @GetMapping("/main")
     fun getMainPage(): String {
         return "mainpage"
     }
 
-    @GetMapping("/article")
-    fun getArticlePage(): String {
+    @GetMapping("/post")
+    fun getArticlePage(@RequestParam id: Long): String {
         return "postpage"
     }
 }
