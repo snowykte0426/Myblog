8a8078a0de2fffa14c29d22c371c46a357d8baa6
snowykte0426 <snowykte0426@naver.com>
2024-12-24 08:29:12 +0900

    feat: 기본 URL 리다이렉션 설정
    기본적인 서버 URL로 요청이 온다면 메인 웹페이지로 리다이렉션 하도록 설정하였습니다

--- src/main/kotlin/com/snowykte0426/myblog/web/WebController.kt
+++ src/main/kotlin/com/snowykte0426/myblog/web/WebController.kt
@@ -3,9 +3,15 @@ package com.snowykte0426.myblog.web
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.servlet.view.RedirectView
 
 @Controller
 class WebController {
+    @GetMapping
+    fun redirectToMain(): RedirectView {
+        return RedirectView("/main")
+    }
+
     @GetMapping("/main")
     fun getMainPage(): String {
         return "mainpage"
