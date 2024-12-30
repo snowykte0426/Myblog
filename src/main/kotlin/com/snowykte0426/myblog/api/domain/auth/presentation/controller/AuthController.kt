fc8be0db2d017cebcd2f6ae9f0cf8f9a029fe4f1
snowykte0426 <snowykte0426@naver.com>
2024-12-30 21:03:28 +0900

    feat: ``/api/v1/auth`` 컨트롤러 추가
    인증/인가를 수행할 예정인 컨트롤러 클래스를 추가하였습니다

---
+++ src/main/kotlin/com/snowykte0426/myblog/api/domain/auth/presentation/controller/AuthController.kt
@@ -0,0 +1,10 @@
+package com.snowykte0426.myblog.api.domain.auth.presentation.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/auth")
+class AuthController {
+
+}
