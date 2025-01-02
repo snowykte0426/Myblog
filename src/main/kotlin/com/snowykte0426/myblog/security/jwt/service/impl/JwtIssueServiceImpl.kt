9ddcee3feffbb65214cfef33c1242745d0c38200
snowykte0426 <snowykte0426@naver.com>
2025-01-02 15:16:43 +0900

    feat: JWT 발급 로직 구현
    JWT를 발급하는 로직들을 구현하였습니다

--- src/main/kotlin/com/snowykte0426/myblog/security/jwt/service/impl/JwtIssueServiceImpl.kt
+++ src/main/kotlin/com/snowykte0426/myblog/security/jwt/service/impl/JwtIssueServiceImpl.kt
@@ -1,17 +1,45 @@
 package com.snowykte0426.myblog.security.jwt.service.impl
 
+import com.snowykte0426.myblog.security.jwt.presentation.dto.TokenDto
 import com.snowykte0426.myblog.security.jwt.service.JwtIssueService
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.*
 
 @Service
 class JwtIssueServiceImpl(
-
+    @Value("\${spring.security.jwt.secret}")
+    val secretKey: String,
+    @Value("\${spring.security.jwt.access-token-expiration}")
+    val accessTokenExpiration: Long,
+    @Value("\${spring.security.jwt.refresh-token-expiration}")
+    val refreshTokenExpiration: Long
 ) : JwtIssueService {
-    override fun issueAccessToken(userId: String): String {
-        TODO("Not yet implemented")
+    private val key = Keys.hmacShaKeyFor(secretKey.toByteArray())
+    override fun issueAccessToken(userId: String): TokenDto {
+        val expirationTime: LocalDateTime = LocalDateTime.now().plusSeconds(accessTokenExpiration)
+        val accessToken: String = Jwts.builder()
+            .setSubject(userId)
+            .setIssuedAt(Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()))
+            .setExpiration(Date.from(expirationTime.atZone(ZoneId.systemDefault()).toInstant()))
+            .signWith(key, SignatureAlgorithm.HS256)
+            .compact()
+        return TokenDto(accessToken, expirationTime)
     }
 
-    override fun issueRefreshToken(userId: String): String {
-        TODO("Not yet implemented")
+    override fun issueRefreshToken(userId: String): TokenDto {
+        val expirationTime: LocalDateTime = LocalDateTime.now().plusSeconds(refreshTokenExpiration)
+        val refreshToken: String = Jwts.builder()
+            .setSubject(userId)
+            .setIssuedAt(Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()))
+            .setExpiration(Date.from(expirationTime.atZone(ZoneId.systemDefault()).toInstant()))
+            .signWith(key, SignatureAlgorithm.HS256)
+            .compact()
+        return TokenDto(refreshToken, expirationTime)
     }
 }
