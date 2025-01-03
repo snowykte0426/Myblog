3daf71645baca77dddaa8101c293686b65cdba87
snowykte0426 <snowykte0426@naver.com>
2025-01-03 19:02:29 +0900

    feat: ``RedisRepository`` 활성화
    Redis를 위하여 사용되는 Repository를 사용하도록 설정해주는 어노테이션을 설정하였습니다

--- src/main/kotlin/com/snowykte0426/myblog/MyBlogApplication.kt
+++ src/main/kotlin/com/snowykte0426/myblog/MyBlogApplication.kt
@@ -2,12 +2,14 @@ package com.snowykte0426.myblog
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
+@EnableRedisRepositories(basePackages = ["com.snowykte0426.myblog.security.jwt.repository"])
 class MyBlogApplication
 
 fun main(args: Array<String>) {
-	runApplication<MyBlogApplication>(*args)
+    runApplication<MyBlogApplication>(*args)
 }
