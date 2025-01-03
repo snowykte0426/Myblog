b99d573e7e2dc8b75308745bc69dfba0c6d25b98
snowykte0426 <snowykte0426@naver.com>
2025-01-04 00:57:36 +0900

    delete: JWT 재발급 로직 제거
    JWT 토큰을 재발급하는 로직이 중복되는 것으로 판단되어 이를 제거하였습니다

--- src/main/kotlin/com/snowykte0426/myblog/security/jwt/service/JwtRefreshService.kt
+++
@@ -1,7 +0,0 @@
-package com.snowykte0426.myblog.security.jwt.service
-
-import com.snowykte0426.myblog.security.jwt.presentation.dto.TokenDto
-
-interface JwtRefreshService {
-    fun refreshAccessToken(refreshToken: String): TokenDto
-}
