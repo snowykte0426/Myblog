593bb54f5db1c8362a360ff0e9749dd62ab6ed76
snowykte0426 <snowykte0426@naver.com>
2025-01-03 19:10:05 +0900

    feat: JWT 파싱 관련 메서드 구현
    JWT 토큰을 검증하고 데이터를 뽑아내는 등의 행위를 하는 메서드들을 구현하였습니다

--- src/main/kotlin/com/snowykte0426/myblog/security/jwt/service/impl/JwtParserServiceImpl.kt
+++ src/main/kotlin/com/snowykte0426/myblog/security/jwt/service/impl/JwtParserServiceImpl.kt
@@ -1,15 +1,40 @@
 package com.snowykte0426.myblog.security.jwt.service.impl
 
+import com.snowykte0426.myblog.security.jwt.component.ExistenceRefreshToken
+import com.snowykte0426.myblog.security.jwt.exception.InvalidTokenException
 import com.snowykte0426.myblog.security.jwt.service.JwtParserService
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
 
+@Service
 class JwtParserServiceImpl(
-
+    @Value("\${spring.security.jwt.secret}")
+    private val secretKey: String,
+    private val existenceRefreshToken: ExistenceRefreshToken
 ) : JwtParserService {
+    private val key = Keys.hmacShaKeyFor(secretKey.toByteArray())
     override fun validateToken(token: String): Boolean {
-        TODO("Not yet implemented")
+        return try {
+            parseToken(token)
+            true
+        } catch (_: Exception) {
+            false
+        }
     }
 
     override fun extractUserId(token: String): String {
-        TODO("Not yet implemented")
+        try {
+            return parseToken(token).subject
+        } catch (_: Exception) {
+            throw InvalidTokenException("Invalid token")
+        }
     }
+
+    override fun validateRefreshToken(token: String): Boolean {
+        return existenceRefreshToken.execute(token)
+    }
+
+    private fun parseToken(token: String) = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).body
 }
